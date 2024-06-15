2dbbf4e105d27f0939eec7f1e0820d2a01457668
tobiashungwe <55983585+tobiashungwe@users.noreply.github.com>
2024-06-15 15:36:18 +0000

    add pop up

--- monet-generator/src/App.tsx
+++ monet-generator/src/App.tsx
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 import ImageUpload from './components/ImageUpload';
 import ImageDisplay from './components/ImageDisplay';
 import SocialMedia from './components/SocialMedia';
+import Popup from './components/Popup';
 import './styles/App.css';
+import './styles/Popup.css';
 
 const App: React.FC = () => {
   const [beforeImage, setBeforeImage] = useState<string>('https://via.placeholder.com/256');
   const [afterImage, setAfterImage] = useState<string>('https://via.placeholder.com/256');
   const [loading, setLoading] = useState<boolean>(false);
+  const [showPopup, setShowPopup] = useState<boolean>(true);
+
+  const handleClose = () => {
+    setShowPopup(false);
+  };
 
   return (
     <div className="App">
+      {showPopup && <Popup onClose={handleClose} />}
       <div className="container">
         <ImageUpload setBeforeImage={setBeforeImage} setAfterImage={setAfterImage} setLoading={setLoading} />
         <ImageDisplay beforeImage={beforeImage} afterImage={afterImage} loading={loading} />
@@ -18,6 +26,6 @@ const App: React.FC = () => {
       <SocialMedia />
     </div>
   );
-};  
+};
 
 export default App;
---
+++ monet-generator/src/components/Popup.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import '../styles//Popup.css';
+
+const Popup = ({ onClose }) => {
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  const slides = [
+    {
+      title: "Welcome! 😊",
+      description: "Our generator creates images in Monet's style using a CycleGAN model. For more details, visit our GitHub page. Click the buttons below to explore the app!",
+    },
+    {
+      images: [
+        '../assets/image_2024-06-15_164442913.png'
+      ],
+      description: "In this app, instead of cats, these were the Monet images. It's trained on nature pictures for best results, but works on any image. Try multiple times for better results.",
+    },
+    {
+      images: ['../assets/image_2024-06-15_164505977.png'],
+      description: "Here we explain how the discriminator and generator work. There are two options: from real to Monet image, or from Monet to real image. But the real Monet generator is not working very well. But should work.",
+    }
+  ];
+
+  const nextSlide = (index) => {
+    setCurrentSlide(index);
+  };
+
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <button className="close-button" onClick={onClose}>X</button>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={currentSlide}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            {currentSlide === 1 && (
+              <div className="slide">
+                <h2>{slides[0].title}</h2>
+                <p>{slides[0].description}</p>
+              </div>
+            )}
+            {currentSlide === 2 && (
+              <div className="slide">
+                <img src={slides[1].images[0]} alt="Slide 2 Image 1" />
+                <p>{slides[1].description}</p>
+              </div>
+            )}
+            {currentSlide === 3 && (
+              <div className="slide">
+                <img src={slides[2].images[0]} alt="Slide 3 Image" />
+                <p>{slides[2].description}</p>
+              </div>
+            )}
+          </motion.div>
+        </AnimatePresence>
+        <div className="radio-buttons">
+          {slides.map((_, index) => (
+            <button
+              key={index}
+              className={`radio-button ${currentSlide === index + 1 ? 'active' : ''}`}
+              onClick={() => nextSlide(index + 1)}
+            ></button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
